4ad44a0afc6c704108b61fce75ac4729280ba51b
codjoelmaer <tmjoelm4@gmail.com>
2023-02-13 20:46:38 +0200

    Make use of sendFile

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+// path
+const path = require('path');
+// db
+const db = require('./config');
+// body-parser
+const bodyParser = require('body-parser');
 // port 
 const port = parseInt(process.env.port) || 4000;
 // Express app
@@ -7,13 +13,14 @@ const app = express();
 const route = express.Router();
 
 app.use(
-    route
+    route,
+    express.json,
+    bodyParser.urlencoded({extended: false}),
 )
 // Home or /
 route.get('/', (req, res)=>{
-    res.status(200).send("Well done");
+    res.status(200).sendFile(path.join(__dirname, './view/index.html'));
 })
-
 app.listen(port, ()=>{
     console.log(`Server is running at ${port}`);
 })
