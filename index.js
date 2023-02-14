1bd14596a6692c225164dc3dcf7ff18bd39f92ef
codjoelmaer <tmjoelm4@gmail.com>
2023-02-14 19:23:54 +0200

    Retrieving data and adding a new user

--- index.js
+++ index.js
@@ -21,6 +21,35 @@ app.use(
 route.get('/', (req, res)=>{
     res.status(200).sendFile(path.join(__dirname, './view/index.html'));
 })
+// Retrieve all users
+route.get('/users', (req, res)=>{
+    const strQry = 
+    `
+    SELECT firstName, lastName, emailAdd, country
+    FROM Users;
+    `;
+    //db
+    db.query(strQry, (err, data)=>{
+        if(err) throw err;
+        res.status(200).json( {result: data} );
+    })
+});
+// Register
+route.post('/register', bodyParser.json(), (req, res)=> {
+    let detail = req.body;
+    console.log(detail);
+    // sql query
+    const strQry =
+    `INSERT INTO Users
+    SET ?;`;
+    db.query(strQry, [detail], (err)=> {
+        if(err) {
+            res.status(400).json({err});
+        }else {
+            res.status(200).json({msg: "A user record was saved."})
+        }
+    })
+})
 app.listen(port, ()=>{
     console.log(`Server is running at ${port}`);
 })
