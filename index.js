e1b63f169aa3c4768353b4c1b3ce2992f54c04a6
codjoelmaer <tmjoelm4@gmail.com>
2023-02-16 16:19:16 +0200

    Update

--- index.js
+++ index.js
@@ -65,8 +65,9 @@ route.get('/users', (req, res)=>{
     `;
     //db
     db.query(strQry, (err, data)=>{
-        if(err) throw err;
-        res.status(200).json( {results: data} );
+        if(err) res.status(400).json({err});
+        else res.status(200).json( 
+            {results: data} );
     })
 });
 // Update
