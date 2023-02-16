c60e5fa3c65aeba2e210fcb669a214f5f6574a4f
codjoelmaer <tmjoelm4@gmail.com>
2023-02-16 20:23:56 +0200

    JT Book Store - Frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2 class="display-2">Home</h2>
+    <p class="lead">
+      Welcome to our book store
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container-fluid">
+        <h2 class="display-2">Login</h2>
+    </div>
+</template>
+  
+<script>
+export default {
+    components: {
+    }
+}
+</script>
