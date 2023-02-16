7ba2212b4776f8d52d576de4f1376c313b9ca373
codjoelmaer <tmjoelm4@gmail.com>
2023-02-16 14:50:32 +0200

    MVC pattern

---
+++ Model/index.js
@@ -0,0 +1,15 @@
+class User {
+    fetchUsers() {
+        console.log("Fetching users");
+    }
+    createUser() {
+        console.log("Create a user");
+    }
+}
+
+
+
+// Export User class
+module.exports = {
+    User
+}
