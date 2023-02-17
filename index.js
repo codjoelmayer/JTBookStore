5c8673573f819378b4012d1545804e9fc9404f04
codjoelmaer <tmjoelm4@gmail.com>
2023-02-17 13:10:09 +0200

    CORS

--- index.js
+++ index.js
@@ -24,16 +24,13 @@ express.json: setting the content-type to application/json
 bodyParser.urlencoded( {extended: true} ): Object will contain
 values of any type instead of just a string
 */
-app.use((req, res, next)=> {
-    res.append('Access-Control-Allow-Origin', ['http://127.0.0.1:8080', 
-    'http://localhost:8080']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 app.use(
     route,
-    cors(),
+    cors({
+        origin: [
+        'http://localhost:8080'],
+        credentials: true
+    }),
     express.json,
     bodyParser.urlencoded({extended: false})
 )
