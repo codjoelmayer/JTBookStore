0294e54a5e21b590b132f82818f8be3c9be02fb8
codjoelmaer <tmjoelm4@gmail.com>
2023-02-17 10:44:06 +0200

    Updates

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,11 +1,43 @@
 <template>
     <div class="container-fluid">
         <h2 class="display-2">Admin</h2>
+        <div class="row">
+            <button class="btn btn-primary">Add a user</button>
+        </div>
+        <div class="row">
+            <table class="table table-hover">
+               <thead class="bg-gradient">
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>Email Address</th>
+                    <th>Country</th>
+                    <th>Edit/Delete</th>
+               </thead>
+               <tbody>
+                    <tr>
+                        {{ users }}
+                    </tr>
+               </tbody>
+            </table>
+        </div>
     </div>
 </template>
 <script>
+import {computed} from '@vue/runtime-core';
+import { useStore } from 'vuex';
 export default {
     components: {
+    },
+    setup() {
+        const store = useStore();
+        store.dispatch("fetchUsers");
+        const users = 
+        computed( ()=>store.state.users );
+
+        return {
+            users,
+        }
     }
+    
 }
 </script>
