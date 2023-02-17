c432badbd47510fa737d899e822676e6d8180100
codjoelmaer <tmjoelm4@gmail.com>
2023-02-17 10:55:33 +0200

    Update

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ values of any type instead of just a string
 app.use(
     route,
     cors({
-        mode: 'no-cors',
         origin: ['http://127.0.0.1:8080', 'http://localhost:8080'],
     credentials: true
     }),
