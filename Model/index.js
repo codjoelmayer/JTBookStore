6e2f2d31d011ceb4def3f58bde2acbc2b1f57088
codjoelmaer <tmjoelm4@gmail.com>
2023-02-18 15:03:07 +0200

    Model

--- Model/index.js
+++ Model/index.js
@@ -1,9 +1,103 @@
+// Database configuration
+const db = require('../config');
 class User {
-    fetchUsers() {
-        console.log("Fetching users");
+    login(req, res) {
+        const {emailAdd, userPass} = req.body;
+        const strQry = 
+        `
+        SELECT firstName, lastName, emailAdd, userPass, 
+        country
+        FROM Users
+        WHERE emailAdd = '${emailAdd}';
+        `;
+        db.query(strQry, (err, data)=>{
+            if(err) throw err;
+            if((!data.length) || (data == null)) {
+                res.status(401).json({err: 
+                    "You provide a wrong email address"});
+            }else {
+                if(userPass === data[0].userPass) {
+                    res.status(200).json(
+                        {result: data[0]});
+                }else {
+                    res.status(200).json({err: 
+                        `You provide a wrong password`});
+                }
+            }
+        })     
     }
-    createUser() {
-        console.log("Create a user");
+    fetchUsers(req, res) {
+        const strQry = 
+        `
+        SELECT firstName, lastName, emailAdd, country
+        FROM Users;
+        `;
+        //db
+        db.query(strQry, (err, data)=>{
+            if(err) throw err;
+            else res.status(200).json( 
+                {results: data} );
+        })
+    }
+    fetchUser(req, res) {
+        const strQry = 
+        `
+        SELECT firstName, lastName, emailAdd, country
+        FROM Users
+        WHERE userID = ?;
+        `;
+        //db
+        db.query(strQry,[req.params.id], 
+            (err, data)=>{
+            if(err) throw err;
+            else res.status(200).json( 
+                {results: data} );
+        })
+
+    }
+    createUser(req, res) {
+        let detail = req.body;
+        // sql query
+        const strQry =
+        `INSERT INTO Users
+        SET ?;`;
+        db.query(strQry, [detail], (err)=> {
+            if(err) {
+                res.status(400).json({err});
+            }else {
+                res.status(200).json({msg: "A user record was saved."})
+            }
+        })    
+    }
+    updateUser(req, res) {
+        let data = req.body;
+        const strQry = 
+        `
+        UPDATE Users
+        SET ?
+        WHERE userID = ?;
+        `;
+        //db
+        db.query(strQry,[data, req.params.id], 
+            (err)=>{
+            if(err) throw err;
+            res.status(200).json( {msg: 
+                "A row was affected"} );
+        })    
+    }
+    deleteUser(req, res) {
+        const strQry = 
+        `
+        DELETE FROM Users
+        WHERE userID = ?;
+        `;
+        //db
+        db.query(strQry,[req.params.id], 
+            (err)=>{
+            if(err) throw err;
+            res.status(200).json( {msg: 
+                "A record was removed from a database"} );
+        })    
     }
 }
 
--- index.js
+++ index.js
@@ -26,6 +26,7 @@ values of any type instead of just a string
 */
 app.use((req, res, next)=> {
     res.setHeader('Access-Control-Allow-Origin', '*')
+    next();
 })
 app.use(
     route,
@@ -44,90 +45,19 @@ route.get('^/$|/jtbookstore', (req, res)=>{
 })
 // Login
 route.patch('/login', bodyParser.json(), (req, res)=>{
-    const {emailAdd, userPass} = req.body;
-    const strQry = 
-    `
-    SELECT firstName, lastName, emailAdd, userPass, 
-    country
-    FROM Users
-    WHERE emailAdd = '${emailAdd}';
-    `;
-    db.query(strQry, (err, data)=>{
-        if(err) throw err;
-        if((!data.length) || (data == null)) {
-            res.status(401).json({err: 
-                "You provide a wrong email address"});
-        }else {
-            if(userPass === data[0].userPass) {
-                res.status(200).json(
-                    {result: data[0]});
-            }else {
-                res.status(200).json({err: 
-                    `You provide a wrong password`});
-            }
-        }
-    }) 
 })
 // Retrieve all users
 route.get('/users', (req, res)=>{
-    const strQry = 
-    `
-    SELECT firstName, lastName, emailAdd, country
-    FROM Users;
-    `;
-    //db
-    db.query(strQry, (err, data)=>{
-        if(err) throw err;
-        else res.status(200).json( 
-            {results: data} );
-    })
+
 });
 // Update
 route.put('/user/:id',bodyParser.json(), (req, res)=>{
-    let data = req.body;
-    const strQry = 
-    `
-    UPDATE Users
-    SET ?
-    WHERE userID = ?;
-    `;
-    //db
-    db.query(strQry,[data, req.params.id], 
-        (err)=>{
-        if(err) throw err;
-        res.status(200).json( {msg: 
-            "A row was affected"} );
-    })
 });
 // Register
 route.post('/register', bodyParser.json(), (req, res)=> {
-    let detail = req.body;
-    // sql query
-    const strQry =
-    `INSERT INTO Users
-    SET ?;`;
-    db.query(strQry, [detail], (err)=> {
-        if(err) {
-            res.status(400).json({err});
-        }else {
-            res.status(200).json({msg: "A user record was saved."})
-        }
-    })
 })
 // Delete
 route.delete('/user/:id', (req, res)=>{
-    const strQry = 
-    `
-    DELETE FROM Users
-    WHERE userID = ?;
-    `;
-    //db
-    db.query(strQry,[req.params.id], 
-        (err)=>{
-        if(err) throw err;
-        res.status(200).json( {msg: 
-            "A record was removed from a database"} );
-    })
 });
 
 app.listen(port, ()=>{
