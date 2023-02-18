02c988e970fa3bc5318e771f6c6a4ae7e402ca18
codjoelmaer <tmjoelm4@gmail.com>
2023-02-19 00:55:34 +0200

    Well done

--- controller/index.js
+++ controller/index.js
@@ -6,9 +6,11 @@ const bodyParser = require('body-parser');
 // Router
 const route = express.Router();
 // Models
-const {User} = require('../model');
+const {User, Product} = require('../model');
 // Create a user instance
 const user = new User();
+// Product instance
+const product = new Product();
 // ^/$|/jtbookstore
 route.get('^/$|/jtbookstore', (req, res)=>{
     res.status(200).sendFile(path.join(__dirname, '../view/index.html'));
@@ -38,5 +40,32 @@ route.post('/register', bodyParser.json(), (req, res)=> {
 route.delete('/user/:id', (req, res)=>{
     user.deleteUser(req, res);
 });
+// =====Products======
+// Fetch all products
+route.get('/products', (req, res)=> {
+    product.fetchProducts(req, res);
+})
+// Fetch a single product
+route.get('/product/:id', 
+(req, res)=> {
+    product.fetchProduct(req, res);
+})
+// Add a new product
+route.post('/product', 
+bodyParser.json(), 
+(req, res)=> {
+    product.addProduct(req, res);
+})
+// Update a product
+route.put('/product/:id', 
+bodyParser.json(),
+(req, res)=> {
+    product.updateProduct(req, res);
+})
+// Delete a product
+route.delete('/product/:id', 
+(req, res)=> {
+    product.deleteProduct(req, res);
+})
 
 module.exports = route;
--- model/index.js
+++ model/index.js
@@ -150,8 +150,77 @@ class User {
     }
 }
 // Product
+class Product {
+    fetchProducts(req, res) {
+        const strQry = `SELECT id, prodName, prodDescription, 
+        levels, prodPrice, prodQuantity, imgURL
+        FROM products;`;
+        db.query(strQry, (err, results)=> {
+            if(err) throw err;
+            res.status(200).json({results: results})
+        });
+    }
+    fetchProduct(req, res) {
+        const strQry = `SELECT id, prodName, prodDescription, 
+        levels, prodPrice, prodQuantity, imgURL
+        FROM products
+        WHERE id = ?;`;
+        db.query(strQry, [req.params.id], (err, results)=> {
+            if(err) throw err;
+            res.status(200).json({results: results})
+        });
 
+    }
+    addProduct(req, res) {
+        const strQry = 
+        `
+        INSERT INTO Products
+        SET ?;
+        `;
+        db.query(strQry,[req.body],
+            (err)=> {
+                if(err){
+                    res.status(400).json({err: "Unable to insert a new record."});
+                }else {
+                    res.status(200).json({msg: "Product saved"});
+                }
+            }
+        );    
+
+    }
+    updateProduct(req, res) {
+        const strQry = 
+        `
+        UPDATE Products
+        SET ?
+        WHERE id = ?
+        `;
+        db.query(strQry,[req.body, req.params.id],
+            (err)=> {
+                if(err){
+                    res.status(400).json({err: "Unable to update a record."});
+                }else {
+                    res.status(200).json({msg: "Product updated"});
+                }
+            }
+        );    
+
+    }
+    deleteProduct(req, res) {
+        const strQry = 
+        `
+        DELETE FROM Products
+        WHERE id = ?;
+        `;
+        db.query(strQry,[req.params.id], (err)=> {
+            if(err) res.status(400).json({err: "The record was not found."});
+            res.status(200).json({msg: "A product was deleted."});
+        })
+    }
+
+}
 // Export User class
 module.exports = {
-    User
+    User, 
+    Product
 }
