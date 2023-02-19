081330dacefff7564d98b8a11562e2c64e0adfc4
codjoelmaer <tmjoelm4@gmail.com>
2023-02-19 18:04:59 +0200

    CORS

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = parseInt(process.env.PORT) || 4000;
 const app = express();
 // Middleware
 const {errorHandling} = require('./middleware/ErrorHandling');
+//
+const cookieParser = require('cookie-parser');
 /*
 express.json: setting the content-type to application/json
 bodyParser.urlencoded( {extended: true} ): Object will contain
@@ -26,6 +28,7 @@ app.use(
         'http://localhost:8080'],
         credentials: true
     }),
+    cookieParser(),
     express.json,
     express.urlencoded({extended: false})
 )
