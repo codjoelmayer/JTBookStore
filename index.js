85aea1d249e1384b9b440f541b042a7a6147cc2d
codjoelmaer <tmjoelm4@gmail.com>
2023-02-21 04:32:53 +0200

    CORS - Updates

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ values of any type instead of just a string
 app.use((req, res, next)=> {
         res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
         res.header("Access-Control-Allow-Credentials", "true")
-        res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+        res.header("Access-Control-Allow-Methods", "*")
+        res.header("Access-Control-Allow-Headers", "*")
         next();
 });
 app.use(route);
